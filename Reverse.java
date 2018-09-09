00540cd7eb98e971f41bb0fb6836a5d12a957ba3
Chahat-kansal <39122702+Chahat-kansal@users.noreply.github.com>
2018-09-09 20:11:02 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Reverse
+{
+    public static void main(String args[])
+    {
+        int n, nu, num=0, rem;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter any Positive Number : ");
+        n = scan.nextInt();
+		
+        nu=n;
+		
+        while(nu != 0)
+        {
+            rem = nu%10;
+            num = num*10 + rem;
+            nu = nu/10;
+        }
+     System.out.println(num);   
+    }
+}
